d6b75e05d31113352bff695f640514d8c470485b
MarDev <Marvinlaixs@gmail.com>
2024-03-01 07:38:15 -0600

    modified:   package-lock.json 	modified:   package.json 	modified:   public/index.html 	modified:   src/App.vue 	new file:   src/components/CrearItem.vue 	new file:   src/components/EditarItem.vue 	new file:   src/components/ListarItem.vue 	modified:   src/main.js 	new file:   src/router/index.js 	new file:   src/views/AboutView.vue 	new file:   src/views/HomeView.vue

---
+++ src/router/index.js
@@ -0,0 +1,45 @@
+import { createRouter, createWebHistory } from "vue-router";
+import HomeView from "../views/HomeView.vue";
+
+import CrearItem from "../components/CrearItem.vue";
+import EditarItem from "../components/EditarItem.vue";
+import ListarItem from "../components/ListarItem.vue";
+
+const routes = [
+  {
+    path: "/",
+    name: "home",
+    component: HomeView,
+  },
+  {
+    path: "/CrearItem",
+    name: "CrearItem",
+    component: CrearItem,
+  },
+  {
+    path: "/EditarItem/:id",
+    name: "EditarItem",
+    component: EditarItem,
+  },
+  {
+    path: "/listaritem",
+    name: "listaritem",
+    component: ListarItem,
+  },
+  {
+    path: "/about",
+    name: "about",
+    // route level code-splitting
+    // this generates a separate chunk (about.[hash].js) for this route
+    // which is lazy-loaded when the route is visited.
+    component: () =>
+      import(/* webpackChunkName: "about" */ "../views/AboutView.vue"),
+  },
+];
+
+const router = createRouter({
+  history: createWebHistory(process.env.BASE_URL),
+  routes,
+});
+
+export default router;
